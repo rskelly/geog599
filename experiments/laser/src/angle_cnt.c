8b99adb84e88811cec7d65bd11d0dd1c937164ea
rob <rob@dijital.ca>
2018-06-30 22:58:23 -0700

    Works.

--- experiments/laser/src/angle_cnt.c
+++ experiments/laser/src/angle_cnt.c
@@ -8,18 +8,15 @@
 #include <inttypes.h>  // uint8_t, etc
 #include <linux/i2c-dev.h> // I2C bus definitions
 #include <signal.h>
+#include <math.h>
 
 int fd;
-char asd_addr = 0b01001000; // Used when addr -> ground
-int val1, val2;
-char writeBuf[3];
-char readBuf[2];
+char asd_addr = 0b01001000; // Used when addr -> ground.
 
 //const float vps = 360.0 / ((5.23 / 6.114) * 32767); // volts per step (derees); a2d's volt scale 
                                                     // goes up to ±6.144V; pi's output is 
                                                     // tied to the input
 int quit = 0;
-float fval;
 
 void sigintHandler(int p) {
   quit = 1;
@@ -28,6 +25,8 @@ void sigintHandler(int p) {
 // fd = file handle
 // addr ain#
 void conf(int fd, int addr, int mode) {
+  char writeBuf[3];
+  char readBuf[2];
   addr = 0b10000000 | (addr << 4) | mode;
   writeBuf[0] = 0b00000001;
   writeBuf[1] = addr;
@@ -54,6 +53,7 @@ void conf(int fd, int addr, int mode) {
 }
 
 int getit() {
+  char readBuf[2];
   if(read(fd, readBuf, 2) != 2) {
       perror("Read conversion");
       exit(-1);
@@ -61,47 +61,62 @@ int getit() {
     return readBuf[0] << 8 | readBuf[1];
 }
 
-
 int main() {
 
+  // Configure the interrupt handler.
   signal(SIGINT, sigintHandler);
 
-  printf("asd_addr: %d\n", asd_addr);
-
+  // Open the device with fd as the handle.
   if((fd = open("/dev/i2c-1", O_RDWR)) < 0) {
     printf("Error: Couldn't open device. %d\n", fd);
     exit(1);
   }
 
+  // Connect to the device.
   if(ioctl(fd, I2C_SLAVE, asd_addr) < 0) {
     printf("Error: Couldn't find device at address %d\n", asd_addr);
     exit(1);
   }
 
-  //writeBuf2[0] = 0b00000001; // 7-0:  [0] config register
-  //writeBuf2[1] = 0b11000000; // 15-0: [15] operational status; [14:12] ain#; [11:9] gain; [8] sigle shot
-  //writeBuf2[2] = 0b10000000; // 7-0:  [7:5] data rate; [4:0] comparators
+  // Configure the device for a continuous read of the input 
+  // voltage on AN1.
+  //conf(fd, 101, 0);
 
-  conf(fd, 101, 1);
-  val1 = getit();
+  // Save up some reads over about five seconds and average the volts.
+  //float v = 0;
+  //for(int i = 0; i < 500; ++i) {
+  //  v += getit();
+  //  usleep(10000);
+ // }
 
+  // The max voltage is the avg of those just read.
+ // float max = v / 500;
+
+  //printf("%f %f\n", max, max / 32676 * 6.144);
+//exit(1);
+  // Configure to read the angle continuously.
   conf(fd, 100, 0);
+
+  float max = 0;
+  int iters = 0;
 
   while(!quit) {
 
-    // ref voltage
-    //val1 = conf(fd, 101);
+    // Read the encoder.
+    float val = getit();
 
-    // encoder
-    val2 = getit();
-
+    if(val > max) max = val;
     printf("\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b");
-    printf("Rotation %07.3f", (float) val2 / val1 * 360);
+    printf("Rotation %07.3f", val / max * 360.0);
 
+    //if(iters++ > 100000)
+    //  max = 0;
+    usleep(1);
   }
 
   printf("\n");
 
+  char writeBuf[3];
   writeBuf[0] = 1;
   writeBuf[1] = 0b11000001; // powe down
   writeBuf[2] = 0b10000101;
