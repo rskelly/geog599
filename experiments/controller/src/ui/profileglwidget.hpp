edd4819dfa68dbea99915a38599d4afcdc9f255a
Rob <rob@dijital.ca>
2019-03-13 12:58:50 -0700

    Scale/style adjustments.

--- experiments/controller/src/ui/profileglwidget.hpp
+++ experiments/controller/src/ui/profileglwidget.hpp
@@ -54,6 +54,7 @@ public:
 		int buf = 5;
 		float dw = maxx - minx;
 		float dh = maxy - miny;
+
 		if(dw > 0 && dh > 0) {
 
 			QPen pen;
@@ -63,11 +64,13 @@ public:
 			int hh = size.height();
 			int h = hh - buf * 2;
 
+			float scale = std::min(w / dw, h / dh);
+
 			p.begin(this);
 			for(const DrawConfig* config : configs) {
 				std::vector<QPointF> pts;
 				for(const auto& it : config->data)
-					pts.emplace_back(buf + ((it.first - minx) / dw) * w, hh - buf - ((it.second - miny) / dh) * h);
+					pts.emplace_back(buf + (it.first - minx) * scale, hh - buf - (it.second - miny) * scale);
 				switch(config->drawType) {
 				case DrawType::Line:
 					pen.setColor(config->lineColor);
@@ -78,9 +81,19 @@ public:
 				case DrawType::Points:
 					pen.setColor(config->lineColor);
 					pen.setStyle(config->lineStyle);
-					pen.setWidth(3);
+					pen.setWidth(1);
 					p.setPen(pen);
 					p.drawPoints(pts.data(), pts.size());
+					break;
+				case DrawType::Cross:
+					pen.setColor(config->lineColor);
+					pen.setStyle(config->lineStyle);
+					pen.setWidth(1);
+					p.setPen(pen);
+					for(QPointF pt : pts) {
+						p.drawLine(pt.x() - 2, pt.y(), pt.x() + 2, pt.y());
+						p.drawLine(pt.x(), pt.y() - 2, pt.x(), pt.y() + 2);
+					}
 					break;
 				}
 				pts.clear();
