410bf8a43395c1660f12b16a9c6b0d7182588c31
rob <rob@dijital.ca>
2019-03-18 16:15:15 -0700

    Add smooth spline from De Boor.

--- experiments/controller/include/math/SmoothSpline.hpp
+++ experiments/controller/include/math/SmoothSpline.hpp
@@ -14,6 +14,72 @@
 
 extern "C" {
 
+	/**
+	c  from  * a practical guide to splines *  by c. de boor
+	calls  setupq, chol1d
+	c
+	c  constructs the cubic smoothing spline  f  to given data  (x(i),y(i)),
+	c  i=1,...,npoint, which has as small a second derivative as possible
+	c  while
+	c  s(f) = sum( ((y(i)-f(x(i)))/dy(i))**2 , i=1,...,npoint ) .le. s .
+	c
+	c******  i n p u t  ******
+	c  x(1),...,x(npoint)   data abscissae,  a s s u m e d  to be strictly
+	c        increasing .
+	c  y(1),...,y(npoint)     corresponding data ordinates .
+	c  dy(1),...,dy(npoint)     estimate of uncertainty in data,  a s s u m-
+	c        e d  to be positive .
+	c  npoint.....number of data points,  a s s u m e d  .gt. 1
+	c  s.....upper bound on the discrete weighted mean square distance of
+	c        the approximation  f  from the data .
+	c
+	c******  w o r k  a r r a y s  *****
+	c  v.....of size (npoint,7)
+	c  a.....of size (npoint,4)
+	c
+	c*****  o u t p u t  *****
+	c  a(.,1).....contains the sequence of smoothed ordinates .
+	c  a(i,j) = f^(j-1)(x(i)), j=2,3,4, i=1,...,npoint-1 ,  i.e., the
+	c        first three derivatives of the smoothing spline  f  at the
+	c        left end of each of the data intervals .
+	c     w a r n i n g . . .   a  would have to be transposed before it
+	c        could be used in  ppvalu .
+	c
+	c******  m e t h o d  ******
+	c     The matrices  Q-transp*d  and  Q-transp*D**2*Q  are constructed in
+	c   s e t u p q  from  x  and  dy , as is the vector  qty = Q-transp*y .
+	c  Then, for given  p , the vector  u  is determined in  c h o l 1 d  as
+	c  the solution of the linear system
+	c               (6(1-p)Q-transp*D**2*Q + p*Q)u  = qty  .
+	c  From  u , the smoothing spline  f  (for this choice of smoothing par-
+	c  ameter  p ) is obtained in the sense that
+	c                        f(x(.))  =  y - 6(1-p)D**2*Q*u        and
+	c                      f''(x(.))  =  6*p*u                      .
+	c     The smoothing parameter  p  is found (if possible) so that
+	c                sf(p)  =  s ,
+	c  with  sf(p) = s(f) , where  f  is the smoothing spline as it depends
+	c  on  p .  if  s = 0, then p = 1 . if  sf(0) .le. s , then p = 0 .
+	c  Otherwise, the secant method is used to locate an appropriate  p  in
+	c  the open interval  (0,1) . However, straightforward application of
+	c  the secant method, as done in the original version of this program,
+	c  can be very slow and is influenced by the units in which  x  and  y
+	c  are measured, as C. Reinsch has pointed out. Instead, on recommend-
+	c  ation from C. Reinsch, the secant method is applied to the function
+	c           g:q |--> 1/sqrt{sfq(q)} - 1/sqrt{s} ,
+	c  with  sfq(q) := sf(q/(1+q)), since  1/sqrt{sfq}  is monotone increasing
+	c  and close to linear for larger  q . One starts at  q = 0  with a
+	c  Newton step, i.e.,
+	c                q_0 = 0,  q_1 = -g(0)/g'(0)
+	c  with  g'(0) = -(1/2) sfq(0)^{-3/2} dsfq, where dsfq = -12*u-transp*r*u ,
+	c  and  u  as obtained for  p = 0 . Iteration terminates as soon as
+	c   abs(sf - s) .le. .01*s .
+	c
+	c     logical test
+	c     parameter (test = .true.)
+	c     integer itercnt
+	 */
+	void smooth_(float* x, float* y, float* dy, int* npoint, float* s, float* v, float* a);
+
 	/*
 	 * Calls in to the fpcurf function of the fitpack (fortran) library.
 	 *
@@ -487,6 +553,41 @@ public:
 		case 5:
 			throw std::runtime_error("d value must be positive.");
 		}
+
+		return true;
+	}
+
+	bool fitDeBoor(const std::vector<P>& pts, double weight, float s) {
+		std::vector<double> weights(pts.size());
+		std::fill(weights.begin(), weights.end(), weight);
+		return fitDeBoor(pts, weights, s);
+	}
+
+	bool fitDeBoor(const std::vector<P>& pts, const std::vector<double>& weights, float s) {
+
+		if(pts.size() != weights.size()) {
+			std::cerr << "Weights and points must have the same length\n";
+			return false;
+		}
+
+		// Inputs
+		int m = (int) pts.size();		// Number of points.
+		std::vector<float> x(m);		// Abscissae
+		std::vector<float> y(m);		// Ordinates
+		std::vector<float> dy(m); 		// Weights
+
+		// Outputs
+		std::vector<float> a(m * 4);
+		std::vector<float> v(m * 7);
+
+		// Copy the values.
+		for(int i = 0; i < m; ++i) {
+			x[i] = pts[i][m_xidx];
+			y[i] = pts[i][m_yidx];
+			dy[i] = weights[i];
+		}
+
+		smooth_(x.data(), y.data(), dy.data(), &m, &s, v.data(), a.data());
 
 		return true;
 	}
