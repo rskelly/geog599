8a562b1d547ecadd47e34a614b25b2055630476b
rob <rob@dijital.ca>
2019-04-11 18:36:07 -0700

    WWork on DS.

--- geog599/scripts/trajectory.py
+++ geog599/scripts/trajectory.py
@@ -31,7 +31,7 @@ def lengthY(p1, p2):
 	'''	
 	return (p1[1] - p2[1]) ** 2.
 
-def hull(pts, dist):
+def hull(pts, dist, clip):
 	'''
 	Constructs a convex hull with the exception that the dist
 	parameter limits the length of a segment in the y dimension.
@@ -41,12 +41,18 @@ def hull(pts, dist):
 	# If there's an alpha do a degenerate hull. It could have been one loop but we save a bit of
 	# time and visual complexity this way.
 	for i in range(2, len(pts)):
-		while len(hull) >= 2 and cross(hull[-2], hull[-1], pts[i]) >= 0 and lengthY(hull[-2], pts[i]) <= (dist * dist):
+		while len(hull) >= 2 and cross(hull[-2], hull[-1], pts[i]) >= 0 and lengthY(hull[-2], pts[i]) <= dist ** 2:
 			hull.pop()
 		hull.append(pts[i])
 
-	hull = np.array(hull)
-	#hull.sort(order = 'y')
+	hull = hull[clip:-(clip + 1)]
+	beg = []
+	end = []
+	c = 3
+	for i in range(c):
+		beg.append((hull[0][0], hull[0][1] - dist * (c - i), hull[0][2]))
+		end.append((hull[-1][0], hull[-1][1] + dist * (i + 1), hull[-1][2]))
+	hull = np.array(beg + hull + end)
 	return hull
 
 def load_points(filename):
@@ -70,7 +76,7 @@ def trajectory(coords, smooth, weight):
 
 
 
-def plot_profile(outfile, smooth, weight, coords, hull, x, alt, vel, acc):
+def plot_profile(outfile, smooth, weight, alpha, coords, hull, x, alt, vel, acc):
 
 	fig = plt.figure(figsize=(12, 4))
 	ax1 = fig.add_subplot(111)
@@ -81,7 +87,7 @@ def plot_profile(outfile, smooth, weight, coords, hull, x, alt, vel, acc):
 
 	ax1.set_ylabel('Elevation (m)')
 	ax1.set_xlabel('Distance (m)')
-	ax1.set_title("Smoothing Cubic Spline (p = {s}, w = {w})".format(s = smooth, w = weight))
+	ax1.set_title("Smoothing Cubic Spline (smooth={s:.3f}, weight={w}, alpha={a})".format(s = smooth, w = weight, a = alpha))
 	for tl in ax1.get_yticklabels():
 	    tl.set_color('g')
 
@@ -101,30 +107,7 @@ def plot_profile(outfile, smooth, weight, coords, hull, x, alt, vel, acc):
 	ymax = max(map(abs, ax3.get_ylim()))
 	ax3.set_ylim((-ymax, ymax))
 
-	plt.legend([l_spl, l_vel, l_acc], ['Altitude (m)', 'Vertical Velocity (m/s)', 'Vertical Acceleration (m/s²)'])
-	plt.savefig(outfile, bbox_inches='tight', format='png', dpi=300)
-	#plt.show()
-	plt.close()
-
-	return (weight, smooth, outfile)
-
-def plot_hull(outfile, alpha, coords, hull):
-	'''
-	Plot the concave hull and point cloud.
-	'''
-	fig = plt.figure(figsize=(12, 4))
-	ax1 = fig.add_subplot(111)
-
-	l_pts, = ax1.plot(coords[...,1], coords[...,2], 'o', color='#cccccc', ms=1)
-	l_hpts, = ax1.plot(hull[...,1], hull[...,2], 'ro', ms=1)
-	l_hseg, = ax1.plot(hull[...,1], hull[...,2], 'l', color='#ff9999', lw=1)
-
-	ax1.set_ylabel('Elevation (m)')
-	ax1.set_xlabel('Distance (m)')
-	for tl in ax1.get_yticklabels():
-	    tl.set_color('g')
-
-	plt.legend([l_pts, l_hpts], ['Point Cloud', 'Hull Vertices'])
+	plt.legend([l_spl, l_vel, l_acc], ['Altitude (m)', 'Velocity (m/s)', 'Acceleration (m/s²)'])
 	plt.savefig(outfile, bbox_inches='tight', format='png', dpi=300)
 	#plt.show()
 	plt.close()
@@ -134,115 +117,135 @@ def plot_hull(outfile, alpha, coords, hull):
 
 def p3angle(p1, p2, p3):
 	'''
-	Return the angle between 3 points. p2 is the middle point.
+	Return the angle between 3 points. p2 is the middle point. Angle is 
+	clampted to 0. <= a <= pi.
 	'''
 	_, x1, y1 = p1
 	_, x2, y2 = p2
 	_, x3, y3 = p3
-	ax = x1 - x2
-	ay = y1 - y2
-	bx = x3 - x2
-	by = y3 - y2
-	return math.atan2(by, bx) - math.atan2(ay, ax)
+	a = math.atan2(y1 - y2, x1 - x2)
+	b = math.atan2(y3 - y2, x3 - x2)
+	if a < 0.:
+		a += math.pi * 2.
+	if b < 0.:
+		b += math.pi * 2.
+	a = b - a if b > a else a - b
+	return a
 
-def weights(pts):
+def weights(pts, exp = 0):
 	'''
-	Compute normalized weights for the pointset based on the angle between
-	triples of points. A 'peak' has a high weight. A 'valley' gets a low weight.
+	Compute weights for the pointset based on the angle between
+	triples of points. An acute angle has a high weight. A flat gets a low weight.
 	'''
-	w = [1.]
+	# Get the std deviation of the points (assumes 3d).
+	std = np.std(pts[...,1])
+	
+	w = [1. / std]
 	for i in range(1, len(pts) - 1):
 		a = p3angle(pts[i - 1], pts[i], pts[i + 1])
-		w.append(math.pow(a / -math.pi, 10.) if a <= 0 else 1.)
-	w.append(1.)
+		w.append(math.pow(a / math.pi, exp) * std)
+	w.append(1. / std)
 	w = np.array(w)
-	#std = np.std(w)
-	#w /= w.max()
-	#w *= std
 	return w
 
-def run_angle_weights(filename, outdir):
+		
+def run(filename, outdir):
 	'''
 	Run the profile with angle-based weights.
 	'''
 
-	smooths = [.1, .5, 1., 2., 3., 4., 5.]
-
+	smooth_mult = [.25, .5, 1., 2., 4.]
+	weight_exp = [0., 1., 2., 4.]
+	alpha = [2., 5., 10., 25., 50.]
+	hull_clip = 5
+	x_clip = 5
+	
+	# Try to create the output directory.
 	try:
 		os.makedirs(outdir)
 	except: pass
 
-	mass = 15.1
+	# Remove files from output.
+	for f in [x for x in os.listdir(outdir) if x.endswith('.png')]:
+		os.unlink(os.path.join(outdir, f))
 
-	tpl = os.path.splitext(os.path.basename(filename))[0] + '_{s}_aw.png'
-	clip = 3
+	# Template for plot output file.
+	ptpl = os.path.splitext(os.path.basename(filename))[0] + '_{s}_{a}_{w}.png'
 
-	for s in smooths:
-		ss = str(s).replace('.', '_')
-		outfile = os.path.join(outdir, tpl.format(s = ss))
-		coords = load_points(filename)
-		chull = hull(coords, 10.)
-		wts = weights(chull)
-		#for i in range(len(wts)):
-		#	print(wts[i], chull[i])
-		spline = trajectory(chull, s, wts)
-		x = np.linspace(chull[0,1], chull[-1,1], 1000)	# Regular x-coords (actually, y)
-		alt = spline(x, 0)								# Altitude
-		vel = spline(x, 1)								# Velocity
-		acc = spline(x, 2)								# Acceleration
-		force = (acc + 9.08665) * mass					# Force (thrust) using mass.
-		plot_profile(outfile, s, '(By Angle)', coords, chull, x[clip:-clip], alt[clip:-clip], vel[clip:-clip], force[clip:-clip]) # Clip the ends -- they tend to have outliers.
+	# Load coordinates from point file.
+	coords = load_points(filename)
+	
+	# Open a stats file. This will contain stats for all runs.
+	with open(os.path.join(outdir, 'stats.csv'), 'w') as f:
+		f.write('filename,alpha,smooth,weight,coord_n,hull_n,above_n,above_max,vel_max,vel_min,acc_max,acc_min,residual\n')
+		
+		for a in alpha:
+			
+			chull = hull(coords, a, hull_clip)
+			minx = chull[0,1]
+			maxx = chull[-1,1]
+			x = np.linspace(minx, maxx, 1000)[x_clip:-(x_clip + 1)]	# Regular x-coords (actually, y)
+			aa = str(a).replace('.', '_')
+	
+			for w in weight_exp:
+				
+				wts = weights(chull, w)
+				ww = str(w).replace('.', '_')
+	
+				for s in smooth_mult:
+	
+					s = math.sqrt(2 * len(chull)) * s
+					ss = '{s:.3f}'.format(s = s).replace('.', '_')
+	
+					spline = trajectory(chull, s, wts)
+					
+					alt = spline(x, 0)								# Altitude
+					vel = spline(x, 1)								# Velocity
+					acc = spline(x, 2)								# Acceleration
+	
+					plotfile = os.path.join(outdir, ptpl.format(s = ss, a = aa, w = ww))
+	
+					plot_profile(plotfile, s, w, a, coords, chull, x, alt, vel, acc) # Clip the ends -- they tend to have outliers.
+
+					# Look at points above the spline.
+					acoords = coords[coords[...,1] >= minx]
+					acoords = acoords[acoords[...,1] <= maxx]
+					above = spline(acoords[...,1], ext = 2)
+	
+					above = acoords[above < acoords[...,2]]
+					above_max = np.max(above)
+					above_n = len(above)
+					
+					vel = spline(acoords[...,1], nu = 1, ext = 2)
+					vel_max = np.max(vel)
+					vel_min = np.min(vel)
+					
+					acc = spline(acoords[...,1], nu = 2, ext = 2)
+					acc_max = np.max(acc)
+					acc_min = np.min(acc)
+					
+					residual = spline.get_residual()
+					
+					coord_n = len(coords)
+					hull_n = len(chull)
+					
+					f.write(','.join(list(map(str, [filename, a, s, w, coord_n, hull_n, above_n, above_max, vel_max, vel_min, acc_max, acc_min, residual]))) + '\n')
 
 
 def run_profiles():
 	with open('profiles.csv', 'r') as f:
 		db = csv.reader(f)
-		head = next(db)
+		next(db)
 		for line in db:
 			if line[0] == '1':
 				print(line[7])
-				run_angle_weights(line[7], 'plots')
-				break
-
-def run_hull():
-	with open('profiles.csv', 'r') as f:
-		db = csv.reader(f)
-		head = next(db)
-		for line in db:
-			if line[0] == '1':
-				print(line[7])
-
-
-				try:
-					os.makedirs(outdir)
-				except: pass
-
-				mass = 15.1
-
-				tpl = os.path.splitext(os.path.basename(filename))[0] + '_{s}_aw.png'
-				clip = 3
-
-				for s in smooths:
-					ss = str(s).replace('.', '_')
-					outfile = os.path.join(outdir, tpl.format(s = ss))
-					coords = load_points(filename)
-					chull = hull(coords, 10.)
-					wts = weights(chull)
-					#for i in range(len(wts)):
-					#	print(wts[i], chull[i])
-					spline = trajectory(chull, s, wts)
-					x = np.linspace(chull[0,1], chull[-1,1], 1000)	# Regular x-coords (actually, y)
-					alt = spline(x, 0)								# Altitude
-					vel = spline(x, 1)								# Velocity
-					acc = spline(x, 2)								# Acceleration
-					force = (acc + 9.08665) * mass					# Force (thrust) using mass.
-					plot(outfile, s, '(By Angle)', coords, chull, x[clip:-clip], alt[clip:-clip], vel[clip:-clip], force[clip:-clip]) # Clip the ends -- they tend to have outliers.
+				run(line[7], 'plots')
 				break
 
 
 if __name__ == '__main__':
 
-	run()
+	run_profiles()
 
 	# p1 = [0, 0, 0]
 	# p2 = [0, 1, 0]
